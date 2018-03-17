c36add3713b3d7b54f3b22fb5958b94f105f95e6
Manolis Savva <manolis.savva@gmail.com>
2018-03-16 18:16:13 -0700

    bump version to v0.5.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='minos-sim',
-      version='0.5.0',
+      version='0.5.1',
       dependency_links=['git+https://github.com/msavva/socketIO-client-2.git@master#egg=socketIO-client-2-0.7.4'],
       install_requires=[
           'socketIO-client-2',
