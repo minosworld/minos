5c93803ed8dc52afa008a9750c9d2385f70290d6
Angel X. Chang <angelx@gmail.com>
2018-12-16 19:59:23 -0800

    Update version to 0.6.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='minos-sim',
-      version='0.5.3',
+      version='0.6.0',
       dependency_links=['git+https://github.com/msavva/socketIO-client-2.git@master#egg=socketIO-client-2-0.7.4'],
       install_requires=[
           'socketIO-client-2==0.7.4',
